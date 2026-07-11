6fb59d1d920b9ae97e3c5e44cc66cfc216e24a41
Lebogang Masia <lebogangmasia0@gmail.com>
2026-07-11 17:31:07 +0200

    Fixed duplicate file

--- src/ImagePanel.java
+++ src/ImagePanel.java
@@ -1,12 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+import java.awt.Graphics;
+import java.awt.Image;
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
 
-/**
- *
- * @author BC-STUDENT
- */
-public class ImagePanel {
-    
+public class ImagePanel extends JPanel {
+    private Image backgroundImage;
+
+    public ImagePanel(String imagePath) {
+        // Load the image safely from your assets package
+        java.net.URL imgURL = getClass().getResource(imagePath);
+        if (imgURL != null) {
+            backgroundImage = new ImageIcon(imgURL).getImage();
+        } else {
+            System.err.println("Could not find image: " + imagePath);
+        }
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        if (backgroundImage != null) {
+            // This ensures the gradient resizes dynamically if the window is stretched
+            g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), this);
+        }
+    }
 }
